64782850df101c2b637ddc070b58db1f399694d1
serje3 <serjerin322@gmail.com>
2024-05-09 01:13:40 +0300

    fixes

--- frontend/src/app/views/uits/uits.module.ts
+++ frontend/src/app/views/uits/uits.module.ts
@@ -1,16 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {UitsRoutingModule} from '@app/views/uits/uits-routing.module';
-import {
-  DissertationsComponent
-} from '@app/views/uits/public/scientific-activity/post-graduate/dissertations/dissertations.component';
 import {HomeComponent} from '@app/views/uits/public/home/home.component';
-import {
-  PracticesComponent
-} from '@app/views/uits/public/scientific-activity/post-graduate/practices/practices.component';
-import {
-  SpecialitiesComponent
-} from '@app/views/uits/public/scientific-activity/post-graduate/specialities/specialities.component';
 import {AboutModule} from '@app/views/uits/public/about/about.module';
 import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
 import { LatestNewsComponent } from './public/home/components/latest-news/latest-news.component';
@@ -23,9 +14,6 @@ import { LatestAnnouncementsComponent } from './public/home/components/latest-an
 @NgModule({
   declarations: [
     HomeComponent,
-    PracticesComponent,
-    SpecialitiesComponent,
-    DissertationsComponent,
     LatestNewsComponent,
     LatestAnnouncementsComponent,
   ],
